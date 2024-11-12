7093eda1beb808ae8fa383e57df41df68742f8dc
graceewat <160866251+graceewat@users.noreply.github.com>
2024-11-12 14:53:12 +0000

    added basic styling to css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
 
         <footer>
           This project was coded by Grace Waterman and is open-sourced on{" "}
