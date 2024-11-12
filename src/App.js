97bb0f60e35553dae3d524cac9ddc55b014c9acd
graceewat <160866251+graceewat@users.noreply.github.com>
2024-11-12 13:58:52 +0000

    Added footer with links to guthub and netlify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-
-
 function App() {
   return (
-    <h1 className="App">Weather App</h1>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Grace Waterman and is open-sourced on{" "}
+        <a
+          href="https://github.com/graceewat/react-weather-app-wk5"
+          target="blank"
+        >
+          Github.
+        </a>{" "}
+        Hosted on{" "}
+        <a href="https://react-weather-app-grace.netlify.app/" target="blank">
+          Netlify.
+        </a>
+      </footer>
+    </div>
   );
 }
-
 export default App;
