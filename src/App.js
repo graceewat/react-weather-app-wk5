9e1a524c91d98b66c2b70b4a807dc6971793b4f4
graceewat <160866251+graceewat@users.noreply.github.com>
2024-11-12 14:09:20 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Grace Waterman and is open-sourced on{" "}
-        <a
-          href="https://github.com/graceewat/react-weather-app-wk5"
-          target="blank"
-        >
-          Github.
-        </a>{" "}
-        Hosted on{" "}
-        <a href="https://react-weather-app-grace.netlify.app/" target="blank">
-          Netlify.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by Grace Waterman and is open-sourced on{" "}
+          <a
+            href="https://github.com/graceewat/react-weather-app-wk5"
+            target="blank"
+          >
+            Github.
+          </a>{" "}
+          Hosted on{" "}
+          <a href="https://react-weather-app-grace.netlify.app/" target="blank">
+            Netlify.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
